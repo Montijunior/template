65f76dc9e4bbff79f25720ce10d4d8c634590b49
Monti junior <montiabouou@gmail.com>
2024-05-18 19:48:52 -0800

    Initial webpack and dependecies set, eslint and prettier to be added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./main.css";
+console.log("hello, world");
+
+function gameChanger() {
+  const hello = "world";
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      title: "Project Template File",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|avif)/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
